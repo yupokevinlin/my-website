88fedfd5eef8c1f36feedf73935f24b207511636
Kevin <yupolin921021@gmail.com>
2021-06-02 02:42:15 -0700

    Added rendering of main page.

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {createStyles, Theme, useTheme} from "@material-ui/core";
 import {Provider} from "react-redux";
 import {configureStore} from "../state/store";
 import {makeStyles} from "@material-ui/core/styles";
+import MainPage from "../display/pages/MainPage/MainPage";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import responsiveFontSizes from "@material-ui/core/styles/responsiveFontSizes";
+import {ThemeProvider} from "@material-ui/styles";
 
 export type AppProps = AppDataProps & AppStyleProps & AppEventProps;
 
@@ -31,7 +35,32 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export namespace AppStore {
+  const getInitialMaterialUITheme = (): Theme => {
+    let theme = createMuiTheme({
+      palette: {
+        primary: {
+          light: "#4dabf5",
+          main: "#2196f3",
+          dark: "#1769aa",
+          contrastText: "#fff",
+        },
+        secondary: {
+          light: "#de464c",
+          main: "#d61820",
+          dark: "#951016",
+          contrastText: "#fff",
+        }
+      },
+      typography: {
+        fontFamily: `"Roboto", sans-serif`,
+      },
+    });
+    theme = responsiveFontSizes(theme);
+    return theme;
+  };
+
   export const store = configureStore();
+  export const theme: Theme = getInitialMaterialUITheme();
 }
 
 const App: React.FC<AppProps> = (props) => {
@@ -44,9 +73,11 @@ const App: React.FC<AppProps> = (props) => {
 
   return (
     <Provider store={AppStore.store}>
-      <div className={classes.app}>
-
-      </div>
+      <ThemeProvider theme={AppStore.theme}>
+        <div className={classes.app}>
+          <MainPage/>
+        </div>
+      </ThemeProvider>
     </Provider>
   );
 };
