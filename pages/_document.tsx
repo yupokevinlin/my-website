5eaea6c6faf8a30342f150e141341fa3c6bcff18
Kevin <yupolin921021@gmail.com>
2021-09-15 02:24:36 -0700

    Fix MUI styles not being loaded.

---
+++ pages/_document.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link id="main-icon" rel="icon" href="./resources/icons/winterIcon.svg" type="image/x-icon"/>
+          <link id="shortcut-icon" rel="shortcut icon" href="./resources/icons/winterIcon.svg" type="image/x-icon"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/>
+          <noscript>You need to enable JavaScript to run this app.</noscript>
+        </Head>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,18 @@ import App from "../src/app/App";
 
 const Index: NextPage = () => {
   return (
-    <div className={"root-container"}>
+    <div className={"root"}>
       <Head>
         <meta charSet="UTF-8"/>
         <meta
           name="viewport"
           content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=yes, minimal-ui"
         />
-        <link id="main-icon" rel="icon" href="./resources/icons/winterIcon.svg" type="image/x-icon"/>
-        <link id="shortcut-icon" rel="shortcut icon" href="./resources/icons/winterIcon.svg" type="image/x-icon"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/>
-        <noscript>You need to enable JavaScript to run this app.</noscript>
         <title>Kevin Lin - Full Stack Developer</title>
       </Head>
-      <body>
-        <div className={"root"}>
-          <App/>
-        </div>
-      </body>
-
+      <App/>
     </div>
-  )
+  );
 }
 
 export default Index;
